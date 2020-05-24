9206daa8fd21be44c719b80560e9ba11fb94db1d
Ashton Sham <ash@Ashtons-MacBook-Pro.local>
2020-05-23 23:06:07 -0400

    05232020 finsihed app

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,8 +1,19 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   //props.videos
-  return <div>{videos.length}</div>;
+  const renderedList = videos.map((video) => {
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
+  });
+
+  return <div className="ui relax divided list">{renderedList}</div>;
 };
 
 export default VideoList;
